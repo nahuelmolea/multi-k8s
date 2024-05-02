513e9e362dfed2ccc971274a988a61122cd90482
Nahuel Olea <nahuel.olea96@gmail.com>
2024-05-02 12:27:23 -0300

    fix default.conf

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>FIB calc</p>
+          <p>FIB calc version 2</p>
           <a
             className="App-link"
             href="https://reactjs.org"
